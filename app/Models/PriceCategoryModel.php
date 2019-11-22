4bdeb43e64610d2cd9308345b16f97bd507d818a
DenSin1592 <wanderer1592@gmail.com>
2019-11-22 13:43:52 +0300

    Created Models

---
+++ app/Models/PriceCategoryModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PriceCategoryModel extends Model
+{
+    protected $table = 'pricelist_categories';
+}
---
+++ app/Models/PriceProductModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PriceProductModel extends Model
+{
+    protected $table = 'pricelist_products';
+}
