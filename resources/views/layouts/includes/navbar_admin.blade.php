e20499b40f522fd8d1bd3e234916a9f16b18dae5
DenSin1592 <wanderer1592@gmail.com>
2019-12-04 11:28:45 +0300

    Changed Links

--- resources/views/layouts/includes/navbar_admin.blade.php
+++ resources/views/layouts/includes/navbar_admin.blade.php
@@ -45,8 +45,8 @@
                                 <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
                                 <ul class="site-menu js-clone-nav d-none d-lg-block">
                                     <li><a href={{route('admin/home')}}>Home</a></li>
-                                    <li><a href={{route('site/about')}}>Категории</a></li>
-                                    <li><a href={{route('site/production')}}>Продукты</a></li>
+                                    <li><a href={{route('admin/categories.index')}}>Категории</a></li>
+                                    <li><a href={{route('admin/productions.index')}}>Продукты</a></li>
                                     <li><a href={{route('auth/home')}}>Log In</a></li>
                                 </ul>
                             </div>
